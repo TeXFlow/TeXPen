0bbf4fd1c7c2c8b0427c87de3e191d6f8768683d
jiha <jihakim@outlook.com>
2025-12-10 12:38:41 -0500

    feat: update rectangle select icon

--- TODO.md
+++ TODO.md
@@ -5,7 +5,6 @@ Bugs
 Behavior
 
 Features
-- [x] Add back rectangle select, allow option between rectangle(default)/lasso just like erasor (refactor)
 
 Visuals
 
--- components/common/icons/ToolbarIcons.tsx
+++ components/common/icons/ToolbarIcons.tsx
@@ -29,13 +29,11 @@ export const SelectIcon = () => (
 );
 
 export const RectSelectIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <rect x="3" y="3" width="18" height="18" rx="2" ry="2" strokeDasharray="4 4" />
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="4" y="4" width="16" height="16" rx="2" strokeDasharray="4 4" /></svg>
 );
 
 export const LassoSelectIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M7 21.5c-4-1.5-5-5-3.5-9s6-7 10-6 6.5 4.5 5 8.5-4 5-8 4-6.5-2.5-5-6.5" />
+        <path d="M10 5C6 5 2 8 3 13C3 16 6 20 10 20C15 20 13 15 16 13C18 12 21 11 21 8C21 5 16 5 10 5Z" strokeDasharray="4 4" />
     </svg>
 );
